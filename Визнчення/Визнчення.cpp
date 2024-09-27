bc002f14991e7a3ad5e0149b61fc2ade58066491
Sashok <Sashok@DESKTOP-ESDN935>
2024-09-26 14:27:16 -1200

    Add project files.

---
+++ Визнчення/Визнчення.cpp
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Програма
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        Console.WriteLine("Введіть букву українського алфавіту у верхньому регістрі:");
+        char буква = Console.ReadKey().KeyChar;
+        Console.WriteLine();
+
+        if (буква >= 'А' && буква <= 'Щ' || буква == 'Ь')
+        {
+            Console.WriteLine($"Наступна буква: {(char)(буква + 1)}");
+        }
+        else if (буква == 'Я')
+        {
+            Console.WriteLine("Далі букв немає.");
+        }
+        else
+        {
+            Console.WriteLine("Невірно введена буква.");
+        }
+    }
+}
